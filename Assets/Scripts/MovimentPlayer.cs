2269d71d63ba9e299cdcef5e7a3dbe6ffea97ac0
saraivas2 <saraivas2@yahoo.com.br>
2025-02-06 22:29:03 -0300

    Atualização Cena1 e animation Player

--- Assets/Scripts/MovimentPlayer.cs
+++ Assets/Scripts/MovimentPlayer.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class MovimentPlayer : MonoBehaviour
 {
     public float velocity;
+    public float forcaPulo;
     private bool travarMouse = true;
     private float mouseX = 0.0f, mouseY = 0.0f;
     float sensibilidade = 1.2f;
+    private Animator animator;
+    private Rigidbody rb;
+    private int walkPlayer = Animator.StringToHash("walking");
+    private int jump = Animator.StringToHash("jump1");
+    private int idleHash = Animator.StringToHash("idle");
+    private int runplyer1 = Animator.StringToHash("running1");
+    float angleY;
+    Vector3 CamVect;
+    Quaternion quat;
+    
+
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>(); 
         if (travarMouse)
         {
             Cursor.visible = false;
@@ -25,15 +39,120 @@ public class MovimentPlayer : MonoBehaviour
 
         transform.eulerAngles = new Vector3(-mouseX, mouseY, 0);
 
+        angleY = transform.eulerAngles.y;
+             
+        CamVect = GetDirectionVector(angleY);
+
         move();
        
     }
 
     private void move()
     {
-        float vert = Input.GetAxis("Vertical");
-        float horiz = Input.GetAxis("Horizontal");
-        transform.Translate(Time.deltaTime * velocity * new Vector3(horiz, 0, vert));
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpPlayer();
+            rb.AddForce(Vector3.up *  forcaPulo * Time.deltaTime, ForceMode.Impulse);
+        
+        } else if (Input.GetKey(KeyCode.W))
+        {
+            movePlayer();
+            transform.Translate(Vector3.forward * Time.deltaTime * velocity);
+
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            movePlayer();
+            transform.Translate(Time.deltaTime * velocity * Vector3.back);
+
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Time.deltaTime * velocity * Vector3.left);
+
+        } else  if (Input.GetKey(KeyCode.D))
+        {
+            movePlayer();
+            transform.Translate(Time.deltaTime * velocity * Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            RunningPlayer();
+            transform.Translate(Vector3.forward * Time.deltaTime * velocity * 2);
+        }
+        else
+        {
+            IdlePlayer();
+            transform.Translate(Vector3.zero);
+        }
     }
 
+    private void movePlayer()
+    {
+        animator.SetBool(jump, false);
+        animator.SetBool(idleHash, false);
+        animator.SetBool(walkPlayer, true);
+        animator.SetBool(runplyer1, false);
+    }
+
+    private void JumpPlayer()
+    {
+        animator.SetBool(jump, true);
+        animator.SetBool(idleHash, false);
+        animator.SetBool(walkPlayer, false);
+        animator.SetBool(runplyer1, false);
+
+    }
+
+
+    private void AttackPlayer()
+    {
+        /*animator.SetBool(jump, false);
+        animator.SetBool(idleHash, true);
+        animator.SetBool(walkPlayer, false);
+        animator.SetBool(runplyer1, false);*/
+
+    }
+
+
+    private void DeathPlayer()
+    {
+        /*barraVida.SetActive(false);
+        animator.SetBool(jump, false);
+        animator.SetBool(idleHash, true);
+        animator.SetBool(walkPlayer, false);
+        animator.SetBool(runplyer1, false);
+       
+        bool resp = gameover.ShowTelaGameOver(true);
+        
+        Invoke("ReloadScene", 3f);*/
+
+    }
+
+    private void ReloadScene()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void IdlePlayer()
+    {
+        animator.SetBool(jump, false);
+        animator.SetBool(idleHash, true);
+        animator.SetBool(walkPlayer, false);
+        animator.SetBool(runplyer1, false);
+    }
+    private void RunningPlayer()
+    {
+        animator.SetBool(jump, false);
+        animator.SetBool(idleHash, false);
+        animator.SetBool(walkPlayer, false);
+        animator.SetBool(runplyer1, true);
+    }
+
+    Vector3 GetDirectionVector(float angleY)
+    {
+        float radianY = angleY * Mathf.Deg2Rad;
+
+        float x = Mathf.Sin(radianY);
+        float z = Mathf.Cos(radianY);
+
+        return new Vector3(x, 0, z);
+    }
 }
